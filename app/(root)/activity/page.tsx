b27ecf2345d9648d482f83c2e55b334ba04ceb79
AtharvGaur <atharava112@gmail.com>
2025-01-31 18:12:21 +0530

    added all pages routes

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,7 @@
+const Activity = async () => {
+    return (
+        <h1>Activity</h1>
+    )
+}
+
+export default Activity
---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,7 @@
+const Search = async () => {
+    return (
+        <h1>Search</h1>
+    )
+}
+
+export default Search
