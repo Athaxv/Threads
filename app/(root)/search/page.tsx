c10168a90035ba26d5fb1a7bf82cc4fe351a64ae
AtharvGaur <atharava112@gmail.com>
2025-01-31 18:36:41 +0530

    haddled all components

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,18 @@
-const Search = async () => {
+import PostThread from "@/components/forms/PostThread";
+import ProfileHeader from "@/components/shared/Profileheader";
+
+import { fetchUser } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+async function Search() {
+    const user = await currentUser();
+    if (!user) return null;
+
+    // fetch organization list created by user
+    const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onboarded) redirect("/onboarding");
     return (
         <h1>Search</h1>
     )
