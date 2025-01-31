d3eb3b27a7ca81618abdc96826bd5cac7f7645c7
AtharvGaur <atharava112@gmail.com>
2025-01-31 14:09:38 +0530

    comment feature

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -9,6 +9,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from 'zod';
 import { Input } from "../ui/input"
+import Image from "next/image"
 
 interface Props {
   threadId: string,
@@ -38,31 +39,36 @@ const Comment = ({ threadId, currentUserImg, currentUserId }: Props) => {
   return (
     <Form {...form}>
       <form
-        className='mt-10 flex flex-col justify-start gap-10'
+        className='comment-form'
         onSubmit={form.handleSubmit(onSubmit)}
       >
         <FormField
           control={form.control}
           name='thread'
           render={({ field }) => (
-            <FormItem className='flex w-full flex-col gap-3'>
-              <FormLabel className='text-base-semibold text-light-2'>
-                Content
+            <FormItem className='flex w-full items-center gap-3'>
+              <FormLabel>
+                <Image
+                  src={currentUserImg}
+                  alt="Profile img"
+                  width={48}
+                  height={48}
+                  className="rounded-full object-cover"
+                />
               </FormLabel>
-              <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+              <FormControl className='border-none bg-transparent'>
                 <Input
                   type="text"
                   placeholder="Comment..."
                   className="no-focus text-light-1 outline-none"
                   {...field} />
               </FormControl>
-              <FormMessage />
             </FormItem>
           )}
         />
 
-        <Button type='submit' className='bg-primary-500'>
-          Post Thread
+        <Button type='submit' className='comment-form_btn'>
+          Reply
         </Button>
       </form>
     </Form>
